f3928b9a40648fe8cd1cbf93782756755dd22ad2
devGOD <devMacbook@devrmbpro.gateway.2wire.net>
2014-09-27 20:52:03 -0400

    Age of Laika rev1

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dogYearsTextField: UITextField!
+    
+    @IBOutlet weak var convertedDogYearsLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertedDogYearsButtonPressed(sender: UIButton) {
+        let dogYearsFormTextField = dogYearsTextField.text.toInt()!
+        
+        let conversionConstant = 7
+        convertedDogYearsLabel.hidden = false
+        convertedDogYearsLabel.text = "\(dogYearsFormTextField + conversionConstant)" + " in Dog Years"
+        dogYearsTextField.resignFirstResponder()
+    }
 
 }
 
